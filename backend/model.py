95872b2f1331bc63eed848559dceee0601502b65
lalithsai <lalithsaidabbiru@gmail.com>
2026-07-06 19:06:55 +0530

    docker added

--- backend/model.py
+++ backend/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torchvision.models import resnet18
+from pathlib import Path
 
 if torch.cuda.is_available():
     DEVICE="cuda"
@@ -9,8 +10,11 @@ else:
 
 model = resnet18(weights=None)
 
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_PATH = BASE_DIR / "best_model.pth"
+
 model.fc = nn.Linear(model.fc.in_features,2)
-model.load_state_dict(torch.load("best_model.pth",map_location=DEVICE))
+model.load_state_dict(torch.load(MODEL_PATH,map_location=DEVICE))
 
 model.to(DEVICE)
 model.eval()
